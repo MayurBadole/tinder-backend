55591aed07f4a1ac73f4fa4e1081a7848bb0b9bc
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-29 12:31:25 +0530

    api call and push data

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
-const connection_url=`mongodb+srv://mayurbadole01:9307543901@cluster0.kvpokhd.mongodb.net/?retryWrites=true&w=majority`
+import Cors from "cors";
+import Cards from "./dbCards.js";
 
 // App config
 const app = express();
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://mayurbadole01:9307543901@cluster0.kvpokhd.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 
 //  Middleeware
+app.use(express.json());
+app.use(Cors());
+// Db Config
 
+mongoose.connect(connection_url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 // api endpoint
 
-app.get('/',(req,res)=>res.status(200).send("hello"))
+app.get("/", (req, res) => res.status(200).send("hello"));
 
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard)
+  .then((data)=>{
+    res.status(201).send(data);
+  })
 
+  .catch((err)=>{
+    res.status(500).send(err)
+  })
+//   Cards.create(dbCard, (err, data) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.status(201).send(data);
+//     }
+//   });
+});
+
+app.get("/tinder/cards", (req, res) => {
+    
+  Cards.find().then((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 // listener
 
-app.listen(port, ()=> console.log(`listen local host:  ${port}`));
+app.listen(port, () => console.log(`listen local host:  ${port}`));
